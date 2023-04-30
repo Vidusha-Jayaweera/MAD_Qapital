129c4c353dd28e669a31c4a559f4d3ed3f667bf6
Vidusha-Jayaweera <it21349560@my.sliit.lk>
2023-05-01 00:46:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/qapital/DebtInsertionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qapital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DebtInsertionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debt_insertion)
+    }
+}
---
+++ app/src/main/java/com/example/qapital/DebtUpdateActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qapital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DebtUpdateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_debt_update)
+    }
+}
