2679f4e2e2f44289c7b234a74dbbffd7ef08d6f0
Vidusha-Jayaweera <it21349560@my.sliit.lk>
2023-05-13 00:28:42 +0530

    Implemented total debt calculation

--- app/src/main/java/com/example/qapital/activities/HomeActivity.kt
+++ app/src/main/java/com/example/qapital/activities/HomeActivity.kt
@@ -3,11 +3,19 @@ package com.example.qapital.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.ImageButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import com.example.qapital.R
+import com.example.qapital.models.DebtModel
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.ktx.auth
 import java.util.Calendar
 
 class HomeActivity : AppCompatActivity() {
@@ -17,8 +25,18 @@ class HomeActivity : AppCompatActivity() {
     private lateinit var UserName: TextView
 
     //firestrore connections
-    private lateinit var auth: FirebaseAuth
+    private var auth: FirebaseAuth = Firebase.auth
     private lateinit var db: FirebaseFirestore
+
+
+    private var user = Firebase.auth.currentUser
+    private val uid = user?.uid //get user id from database
+    private var dbRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("Debts")
+
+    //initialize var for storing amount value from db
+    var allTimeExpense: Double = 0.0
+    var allTimeIncome: Double = 0.0
+    var allTimeDebt: Double = 0.0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +44,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         // Initialize Firebase instances
-        auth = FirebaseAuth.getInstance()
+//        auth = FirebaseAuth.getInstance()
         db = FirebaseFirestore.getInstance()
 
         // Initialize button
@@ -35,6 +53,8 @@ class HomeActivity : AppCompatActivity() {
         // Initialize TextView
         GreatingText = findViewById(R.id.GreatingText)
         UserName = findViewById(R.id.NameUser)
+
+        fetchAmount()
 
         // Get the current user's ID
         val userId = auth.currentUser?.uid
@@ -69,5 +89,53 @@ class HomeActivity : AppCompatActivity() {
             else -> "Good Evening"
         }
     }
+
+    private fun showAllTimeRecap() {
+
+        val tvNetAmount: TextView = findViewById(R.id.netAmount)
+        val tvAmountExpense: TextView = findViewById(R.id.expenseAmount)
+        val tvAmountIncome: TextView = findViewById(R.id.incomeAmount)
+        val tvAmountDebt: TextView = findViewById(R.id.debtAmount)
+
+//        tvNetAmount.text = "${allTimeIncome-allTimeExpense}"
+//        tvAmountExpense.text = "$allTimeExpense"
+//        tvAmountIncome.text = "$allTimeIncome"
+          tvAmountDebt.text = "$allTimeDebt"
+    }
+
+    private fun fetchAmount() {
+        var amountExpenseTemp = 0.0
+        var amountIncomeTemp = 0.0
+        var amountDebtTemp = 0.0
+
+        val debtList: ArrayList<DebtModel> = arrayListOf<DebtModel>()
+
+        dbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                debtList.clear()
+                if (snapshot.exists()) {
+                    for (debtSnap in snapshot.children) {
+                        val debtData = debtSnap.getValue(DebtModel::class.java)
+                        debtList.add(debtData!!)
+                    }
+                }
+
+                //take all amount expense, income and debts:
+                for ((i) in debtList.withIndex()){
+                    amountDebtTemp += debtList[i].debtAmount!!
+                }
+                allTimeDebt = amountDebtTemp
+
+                showAllTimeRecap()
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
 }
 
